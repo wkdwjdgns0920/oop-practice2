49c52380a27bf2de728e86ab3e5a918bd4b78bf6
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2024-08-08 18:32:23 +0900

    사칙연산계산기 구현2

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,8 +2,15 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * 요구사항
@@ -12,25 +19,45 @@ import static org.assertj.core.api.Assertions.assertThat;
  * MVC패턴(Model-View-Controller) 기반으로 구현한다.
  */
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CalculatorTest {
 
     // 1 + 2 ----> Calculator
     //  3   <----
+
+    /**
+     * @DisplayName("덧셈 연산을 수행한다.")
+     * @Test void additionTest() {
+     * <p>
+     * int result = Calculator.calculate(1, "+", 2);
+     * <p>
+     * assertThat(result).isEqualTo(3);
+     * }
+     * @DisplayName("뺄셈 연산을 수행한다.")
+     * @Test void subtractionTest() {
+     * <p>
+     * int result = Calculator.calculate(1, "-", 2);
+     * <p>
+     * assertThat(result).isEqualTo(-1);
+     * }
+     **/
+
     @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void additionTest() {
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void additionTest(int operand1, String operator, int operand2, int result) {
 
-        int result = Calculator.calculate(1, "+", 2);
+        int calculateResult = Calculator.calculate(operand1, operator, operand2);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculateResult).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산을 수행한다.")
-    @Test
-    void subtractionTest() {
-
-        int result = Calculator.calculate(1, "-", 2);
-
-        assertThat(result).isEqualTo(-1);
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
     }
 }
