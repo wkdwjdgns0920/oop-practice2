8d75ef54ff422f3051d243676ea341e5f13c2402
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2024-08-08 17:49:30 +0900

    사칙연산계산기 구현

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 요구사항
+ * 간단한 사칙연산을 할 수 있다.
+ * 나눗셈에서 NBSP 8을 나누는 경우 IllegalArgument 예외를 발생시킨다.
+ * MVC패턴(Model-View-Controller) 기반으로 구현한다.
+ */
+
+public class CalculatorTest {
+
+    // 1 + 2 ----> Calculator
+    //  3   <----
+    @DisplayName("덧셈 연산을 수행한다.")
+    @Test
+    void additionTest() {
+
+        int result = Calculator.calculate(1, "+", 2);
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("뺄셈 연산을 수행한다.")
+    @Test
+    void subtractionTest() {
+
+        int result = Calculator.calculate(1, "-", 2);
+
+        assertThat(result).isEqualTo(-1);
+    }
+}
