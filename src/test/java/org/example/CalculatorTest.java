f36818c68db7d6dc7973b22273eef747f9185a8f
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2024-08-09 10:27:39 +0900

    사칙연산 계산기3

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -10,12 +11,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * 요구사항
  * 간단한 사칙연산을 할 수 있다.
- * 나눗셈에서 NBSP 8을 나누는 경우 IllegalArgument 예외를 발생시킨다.
+ * 양수로만 계산할 수 있다.
+ * 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
  * MVC패턴(Model-View-Controller) 기반으로 구현한다.
  */
 
@@ -47,7 +50,7 @@ public class CalculatorTest {
     @MethodSource("formulaAndResult")
     void additionTest(int operand1, String operator, int operand2, int result) {
 
-        int calculateResult = Calculator.calculate(operand1, operator, operand2);
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
         assertThat(calculateResult).isEqualTo(result);
     }
